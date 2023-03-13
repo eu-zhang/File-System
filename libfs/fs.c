0020c8d2a86986c2e1a9210b66d8488cb65ab06a
adeel <leedademha@gmail.com>
2023-03-12 20:16:28 -0700

    Added fs_create, fs_delete, fs_ls. Need to add More error checking If the file is invalid

--- libfs/fs.c
+++ libfs/fs.c
@@ -42,26 +42,33 @@ struct rootdir
 
 struct superblock sb;
 struct FAT fat;
+struct rootdir root;
+struct file_entry fEntry;
+int mounted = 0;
 
 int fs_mount(const char *diskname)
 {
-	/* open the virtual disk, using the block API */ 
-	if (block_disk_open(diskname) == -1)  {
+	/* open the virtual disk, using the block API */
+	if (block_disk_open(diskname) == -1)
+	{
 		return -1;
 	}
 
 	/* load the meta-information */
-	if (block_read(0, &sb) == -1) {
+	if (block_read(0, &sb) == -1)
+	{
 		return -1;
 	}
 
 	/* validate signature of the superblock is ECS150FS */
-	if (strcmp(sb.signature, "ECS150FS") != 0) {
+	if (strcmp(sb.signature, "ECS150FS") != 0)
+	{
 		return -1;
 	}
 
 	/* initialize FAT */
-	if (block_disk_count() != sb.num_data_blocks) {
+	if (block_disk_count() != sb.num_data_blocks)
+	{
 		return -1;
 	}
 
@@ -71,17 +78,18 @@ int fs_mount(const char *diskname)
 	/* load FAT blocks */
 	uint16_t *block = malloc(BLOCK_SIZE); // block index of first FAT block
 
-	for (int i = 1; i < sb.num_FAT_blocks; i++) {
+	for (int i = 1; i < sb.num_FAT_blocks; i++)
+	{
 		// index 0 of fat is EOC
-		if (block_read(i, block) == -1) {
+		if (block_read(i, block) == -1)
+		{
 			return -1;
 		}
 		// copy block into fat entries array
-		memcpy(fat.entries + (i * BLOCK_SIZE / 2), block, BLOCK_SIZE);		
+		memcpy(fat.entries + (i * BLOCK_SIZE / 2), block, BLOCK_SIZE);
 	}
-
+	mounted = 1;
 	return 0;
-
 }
 
 int fs_umount(void)
@@ -89,12 +97,12 @@ int fs_umount(void)
 	/* TODO: Phase 1 */
 	free(fat.entries);
 
-	if (block_disk_close() == -1) {
+	if (block_disk_close() == -1)
+	{
 		return -1;
 	}
 
 	return 0;
-
 }
 
 int fs_info(void)
@@ -105,16 +113,80 @@ int fs_info(void)
 int fs_create(const char *filename)
 {
 	/* TODO: Phase 2 */
+	if (!mounted)
+	{
+		return -1;
+	}
+
+	// Loop through directory and check if file exists or empty entry exists
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++)
+	{
+		if (strcmp(root.entries[i].file_name, filename) == 0)
+		{
+			return -1;
+		}
+		else if (root.entries[i].file_name == "")
+		{
+			// Empty entry exists create a new blank file in entry
+			memcpy(root.entries[i].file_name, filename, sizeof(fEntry));
+			root.entries[i].file_size = 0;
+			root.entries[i].first_data_block = FAT_EOC;
+			return 0;
+		}
+	}
+
+	return -1;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	if (!mounted)
+	{
+		return -1;
+	}
+
+	// Loop through root directory and look for filename
+
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++)
+	{
+		if (strcmp(root.entries[i].file_name, filename) == 0)
+		{
+			memcpy(root.entries[i].file_name, "", sizeof(fEntry));
+			int curBlock = root.entries[i].first_data_block;
+			for (int i = 0; i < fat.num_entries; i++)
+			{
+				if (curBlock == FAT_EOC)
+				{
+					break;
+				}
+				fat.entries[curBlock] = 0;
+				curBlock = fat.entries[curBlock + 1];
+			}
+			return 0;
+		}
+	}
+
+	return -1;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+	if (!mounted)
+	{
+		return -1;
+	}
+
+	int curFile = 0;
+	while (!strcmp(root.entries[curFile].file_name, "") == 0)
+	{
+		printf("file: %s, ", root.entries[curFile].file_name);
+		printf("size: %d, ", root.entries[curFile].file_size);
+		printf("data block: %d", root.entries[curFile].first_data_block);
+	}
+
+	return 0;
 }
 
 int fs_open(const char *filename)
