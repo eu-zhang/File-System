97a0d467214406372501bdf2bba166cb1fd2d32b
Eugenia <eugenia.cc.zhang@gmail.com>
2023-03-12 17:31:50 -0700

    fs_mount and fs_unmount

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,6 +8,7 @@
 #include "fs.h"
 
 #define FAT_EOC 0xFFFF
+#define BLOCK_SIZE 4096
 
 struct superblock {
 	char signature[8];			// must be equal to “ECS150FS”
@@ -35,17 +36,61 @@ struct rootdir {
 	struct file_entry entries[128];
 };
 
-/* TODO: Phase 1 */
+struct superblock sb;
+struct FAT fat;
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
-	/* open the virtual disk, using the block API, and load the meta-information that is necessary to handle the file system operations */
+	/* open the virtual disk, using the block API */ 
+	if (block_disk_open(diskname) == -1)  {
+		return -1;
+	}
+
+	/* load the meta-information */
+	if (block_read(0, &sb) == -1) {
+		return -1;
+	}
+
+	/* validate signature of the superblock is ECS150FS */
+	if (strcmp(sb.signature, "ECS150FS") != 0) {
+		return -1;
+	}
+
+	/* initialize FAT */
+	if (block_disk_count() != sb.num_data_blocks) {
+		return -1;
+	}
+
+	fat.num_entries = sb.num_data_blocks;
+	fat.entries = malloc(fat.num_entries * sizeof(uint16_t));
+
+	/* load FAT blocks */
+	uint16_t *block = malloc(BLOCK_SIZE); // block index of first FAT block
+
+	for (int i = 1; i < sb.num_FAT_blocks; i++) {
+		// index 0 of fat is EOC
+		if (block_read(i, block) == -1) {
+			return -1;
+		}
+		// copy block into fat entries array
+		memcpy(fat.entries + (i * BLOCK_SIZE / 2), block, BLOCK_SIZE);		
+	}
+
+	return 0;
+
 }
 
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
+	free(fat.entries);
+
+	if (block_disk_close() == -1) {
+		return -1;
+	}
+
+	return 0;
+
 }
 
 int fs_info(void)
